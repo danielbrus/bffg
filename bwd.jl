1efd832a438cbec2b95d24bf0b6a0afff71e85d7
brusda <44020246+brusda@users.noreply.github.com>
2021-03-17 11:10:03 +0100

    Add files via upload

--- bwd.jl
+++ bwd.jl
@@ -1,44 +1,53 @@
-#h3 = Array{Float64,1}(undef, 3)  # pre allocate everything?
-
-#=
-function Backward!(Κ, h, hout)  # avoid allocations?
-    hout = K*h
-end
-=#
+using BenchmarkTools
 using StatsBase
 
-struct hTransform
-    Likelihood::Vector
+struct HTransform{T}
+    Likelihood::Vector{T}
 end
 
 struct Message
-    h::hTransform
-    #honto::hTransform
+    h::HTransform
+    #honto::HTransform
 end
 
-struct Kernel
-    TransitionMatrix::Matrix
+struct Kernel{T<:AbstractFloat}
+    TransitionMatrix::Matrix{T}
 end
 
-struct Probability
-    Distribution::Array
+struct Probability{T<:Number}
+    p::Array{T}
+    #function Probability(a::Array{T}) where T
+    #    new{T}(a/sum(a))
+    #end
 end
 
-function Backward(Κ::Kernel, h::hTransform) #specify return :: type ?
-    hout = hTransform(K.TransitionMatrix*h.Likelihood)
+function backward(Κ::Kernel, h::HTransform) #specify return :: type ?
+    hout = HTransform(K.TransitionMatrix*h.Likelihood)
     Message(h), hout
 end
 
-function Forward(Κ::Kernel, m::Message, μ::Probability)
+function changeofmeasure(K::Kernel, m::Message)
+    K_star = K.TransitionMatrix .* transpose(m.h.Likelihood)
+    w = sum(K_star, dims=2)
+    K_star = K_star ./ replace!(w, 0=>1)
+    Kernel(K_star)
+end
+
+function forward(Κ::Kernel, m::Message, μ::Probability)
     ## Julia best practice: Should i Initialise a new K_star = Kernel(K.Trans....) ?
+    #=
     K_star = Κ.TransitionMatrix .* transpose(m.h.Likelihood)
     w = sum(K_star, dims=2)
     K_star = K_star ./ replace!(w, 0=>1)   # if row sums to 0 dont div by 0
-    μout = Probability(μ.Distribution * K_star)
+    =#
+    μout = Probability(μ.p * changeofmeasure(K, m).TransitionMatrix)
+
+    #μout = Probability(μ.p * K_star)
     μout
 end
 
-function Forward(Κ::Kernel, m::Message, x::Int64)
+#=
+function forward(Κ::Kernel, m::Message, x::Integer)
     ## Julia best practice: Should i Initialise a new K_star = Kernel(K.Trans....) ?
     K_star = Κ.TransitionMatrix .* transpose(m.h.Likelihood)
     ## this whole function is kind of 'unfunctional' not sure if this is inline with paradigm
@@ -47,36 +56,60 @@ function Forward(Κ::Kernel, m::Message, x::Int64)
     xout = sample(Weights(K_star[x,:]))
     xout
 end
+=#
 
-elementwise(ha::hTransform, hb::hTransform) = ha.Likelihood .* hb.Likelihood
+elementwise(ha::HTransform, hb::HTransform) = ha.Likelihood .* hb.Likelihood
 
-function Fusion(branches)
-    hout = reduce(elementwise, branches)   #or hTransform(reduce ... ) ?
-    hTransform(hout)
+function fusion(branches)
+    hout = reduce(elementwise, branches)   #or HTransform(reduce ... ) ?
+    HTransform(hout)
 end
 
 K = Kernel([3/4    1/4  0
             0      1/2  1/2
             1/10   0    9/10])
 
-h4a = hTransform([0, 0, 1]) #x4a = '3'
-h4b = hTransform([0, 1, 0]) #x4b = '2'
+#=
+@btime begin
+    h4a = HTransform([0.0, 0.0, 1.0]) #x4a = '3'
+    m4a, h34a = backward(K, h4a)
+end
+=#
 
-m4a, h34a = Backward(K, h4a)
-m4b, h34b = Backward(K, h4b)
+h4a = HTransform([0.0, 0.0, 1.0]) #x4a = '3'
+m4a, h34a = backward(K, h4a)
 
-h3 = Fusion([h34a, h34b])
+h4b = HTransform([0.0, 1.0, 0.0]) #x4b = '2'
+m4b, h34b = backward(K, h4b)
 
-m3, h2 = Backward(K, h3)
-m2, h1 = Backward(K, h2)
-m1, h0 = Backward(K, h1)
+#=
+@btime h3 = fusion([h34a, h34b])
+=#
+
+h3 = fusion([h34a, h34b])
+
+m3, h2 = backward(K, h3)
+m2, h1 = backward(K, h2)
+m1, h0 = backward(K, h1)
 
 μ0 = Probability([1/3 1/3 1/3])
+
 #μ0 = 1
 
-μ1 = Forward(K, m1, μ0)
-μ2 = Forward(K, m2, μ1)
-μ3 = Forward(K, m3, μ2)
+#=
+@btime μ1 = forward(K, m1, μ0)
+=#
 
-μ4a = Forward(K, m4a, μ3)
-μ4b = Forward(K, m4b, μ3)
+μ1 = forward(K, m1, μ0)
+μ2 = forward(K, m2, μ1)
+μ3 = forward(K, m3, μ2)
+
+μ4a = forward(K, m4a, μ3)
+μ4b = forward(K, m4b, μ3)
+
+println(μ0)
+println(μ1)
+println(μ2)
+println(μ3)
+println(μ4a)
+println(μ4b)
